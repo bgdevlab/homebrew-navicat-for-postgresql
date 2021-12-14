c57f107b5164435156d357803eaea66343ba0d20
Theodson <theodson@gmail.com>
2021-12-15 00:09:42 +1100

    add cask navicat-for-postgresql-12

---
+++ navicat-for-postgresql-12.rb
@@ -0,0 +1,11 @@
+cask "navicat-for-postgresql-12" do
+  version "12.1.31"
+  sha256 "75b39291035a6eaf8d3b45afee8282ae6e9a94f41dd8f4a058dcc0f29f53b134"
+
+  url "https://download3.navicat.com/download/navicat121_pgsql_en.dmg"
+  name "Navicat for PostgreSQL"
+  desc "Navicat for PostgreSQL 12"
+  homepage "https://www.navicat.com/products/navicat-for-postgresql"
+
+  app "Navicat for PostgreSQL.app"
+end
