1f6938eda1fc4aaf1d794ecc616036e0a9be236f
Theo Dickinson <theodson@gmail.com>
2022-04-06 23:09:53 +1000

    Update navicat-for-postgresql-12.rb

--- Casks/navicat-for-postgresql-12.rb
+++ Casks/navicat-for-postgresql-12.rb
@@ -1,6 +1,6 @@
 cask "navicat-for-postgresql-12" do
   version "12.1.31"
-  sha256 "75b39291035a6eaf8d3b45afee8282ae6e9a94f41dd8f4a058dcc0f29f53b134"
+  sha256 "a16183450f56f787eea212675434b8baa414b43bc121a98229bbd121efa90fb5"
 
   url "https://download3.navicat.com/download/navicat121_pgsql_en.dmg"
   name "Navicat for PostgreSQL"
